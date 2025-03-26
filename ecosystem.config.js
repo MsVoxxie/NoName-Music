d3a23af235a561775ee8bc34ac38fbcc4f44882a
MsVoxxie <odysseylove222@gmail.com>
2025-03-26 15:15:03 -0400

    more ignore

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			name: 'noNameMusic',
 			script: './noNameMusic.js',
 			watch: true,
-			ignore_watch: ['*-base.js', 'node_modules'],
+			ignore_watch: ['*-base.js', 'node_modules', '.git', 'package-lock.json', 'package.json'],
 		},
 	],
 };
