2681d8a77dacfc998a1a2c87f588c0f7fd3b5ee6
MsVoxxie <odysseylove222@gmail.com>
2025-05-03 18:11:50 -0400

    hfgsg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			name: 'noNameMusic',
 			script: './noNameMusic.js',
 			watch: true,
-			ignore_watch: ['*-base.js', 'node_modules', '.git', 'package-lock.json', 'package.json'],
+			ignore_watch: ['*-base.js', '*-player-script.js', 'node_modules', '.git', 'package-lock.json', 'package.json'],
 		},
 	],
 };
